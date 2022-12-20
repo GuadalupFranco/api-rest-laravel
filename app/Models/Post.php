0c55fb6d45429fd3d87f4736208adfd6e3b0042b
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-12-20 14:09:06 -0600

    add tailwind library

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected function excerpt(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => Str::limit($this->content, 120, '...')
+        );
+    }
+
+    protected function publishedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->created_at->diffForHumans()
+            /* $this->created_at->format('d/m/y h:s:m') */
+        );
+    }
+    
 }
